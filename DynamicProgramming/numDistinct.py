a9367f6eb2f63179b788113c84a9ae9a4938c7f6
zihao-cpu <68882271+zihao-cpu@users.noreply.github.com>
2024-11-22 23:23:07 +0800

    Add files via upload

---
+++ DynamicProgramming/numDistinct.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   numDistinct.py
+@Time    :   2024/11/22 22:27:33
+@Author  :   Zihao Zheng 
+@Email   :   zhengzihao718@163.com
+'''
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0] * (len(t)+1) for _ in range(len(s)+1)]
+        for i in range(len(s)):
+            dp[i][0] = 1
+        for j in range(1, len(t)):
+            dp[0][j] = 0
+        for i in range(1, len(s)+1):
+            for j in range(1, len(t)+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[-1][-1]
