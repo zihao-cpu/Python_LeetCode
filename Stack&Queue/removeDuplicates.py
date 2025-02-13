30cc24414ef3905b0f4b910d9d9a8868a209c347
zihao-cpu <68882271+zihao-cpu@users.noreply.github.com>
2025-02-13 20:48:26 +0800

    Create removeDuplicates.py

---
+++ Stack&Queue/removeDuplicates.py
@@ -0,0 +1,11 @@
+# 方法一，使用栈
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        res = list()
+        for item in s:
+            if res and res[-1] == item:
+                res.pop()
+            else:
+                res.append(item)
+        return "".join(res)  # 字符串拼接
